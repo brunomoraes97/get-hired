33ce2cb2876b2062d4af7d7561abde51995b07da
brunomoraes97 <1997mbrunomoraes@gmail.com>
2025-07-21 02:37:55 -0300

    improve readability and ux

--- flaskr/create_fake_user.py
+++ flaskr/create_fake_user.py
@@ -1,6 +1,9 @@
 import os
 from werkzeug.security import generate_password_hash
 from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 url = os.environ.get('SUPABASE_URL')
 key = os.environ.get('SUPABASE_KEY')
